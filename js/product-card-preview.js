ed67685376df203a2a25c4cf7abace5f4adb674a
VladZank0 <vlad.zanko.2019@mail.ru>
2024-08-10 16:28:26 +0300

    fix poduct-card script

--- js/product-card-preview.js
+++ js/product-card-preview.js
@@ -1,13 +1,61 @@
 (function(){
 
     let cards = document.querySelectorAll('.product-card');
-    
+   
+
+   
     
     cards.forEach((card) => {
         let separators = card.querySelectorAll('.product-card-preview__separator-item');
         let preview = card.querySelector('.product-card-preview__track');
         let pagination = card.querySelector('.product-card-preview__pagination');
         let pagination_bullets = card.querySelectorAll('.bullet');
+
+        let touchStartX = 0;
+        let touchEndX = 0;
+
+        function handleSwipe(preview, pagination) {
+            if (touchEndX < touchStartX - 50) {
+                // Свайп влево
+                console.log('right')
+                showNextSlide(preview, pagination);
+            
+            } else if (touchEndX > touchStartX + 50) {
+                // Свайп вправо
+                console.log('left')
+                showPreviousSlide(preview, pagination);
+            }
+        }
+    
+        function showNextSlide(preview, pagination) {
+            const activeSlide = preview.querySelector('.product-card-preview__slide_active');
+            const nextSlide = activeSlide.nextElementSibling;
+            if (nextSlide) {
+            activeSlide.classList.remove('product-card-preview__slide_active');
+            nextSlide.classList.add('product-card-preview__slide_active');
+              updatePagination(preview, pagination);
+            }
+        }
+    
+        function showPreviousSlide(preview, pagination) {
+            const activeSlide = preview.querySelector('.product-card-preview__slide_active');
+            const previousSlide = activeSlide.previousElementSibling;
+            if (previousSlide) {
+                activeSlide.classList.remove('product-card-preview__slide_active');
+                previousSlide.classList.add('product-card-preview__slide_active');
+                updatePagination(preview, pagination);
+            }
+        }
+    
+        function updatePagination(preview, pagination) {
+            let currentImage =  preview.querySelector('.product-card-preview__slide_active').getAttribute('data-image');
+            pagination.querySelectorAll('.bullet').forEach((e) => {
+                e.classList.remove('bullet_active');
+            })
+            pagination.querySelector(`[data-image="${currentImage}"`).classList.add('bullet_active');
+
+        }
+        
         separators.forEach((separator) => {
             separator.addEventListener('mouseenter', ()=>{
                 preview.querySelectorAll('.product-card-preview__slide').forEach((e) => {
@@ -36,6 +84,18 @@
 
             })
         })
+
+
+        //для мобилки
+
+        preview.addEventListener('touchstart', (event) => {
+            touchStartX = event.changedTouches[0].screenX;
+        });
+        preview.addEventListener('touchend', (event) => {
+            touchEndX = event.changedTouches[0].screenX;
+            handleSwipe(preview, pagination);
+        });
     })
 
 }())
+
