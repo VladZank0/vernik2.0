63cb558d9267c7e941e816581790af3813c63355
VladZank0 <vlad.zanko.2019@mail.ru>
2024-10-23 15:30:37 +0300

    geo_modal fix and others

--- js/fancybox.js
+++ js/fancybox.js
@@ -1,4 +1,17 @@
 Fancybox.bind("[data-fancybox]", {
-     
+    on: {
+        reveal: (fancybox, slide) => {
+            // Проверяем, какая модалка открыта
+            if (slide.src === "#geo") {
+                // Задаем свойства для модалки 1
+                document.querySelector('.with-fancybox').style.overflow = "auto";
+                document.querySelector('.fancybox__backdrop').style.display = "none";
+
+            }
+        },
+    },
+    Hash: false,
 });
-Fancybox.defaults.Hash = false;
+
+
+
